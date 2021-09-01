8be64e80801aaf11a2910751a76563be1a8816b3
agonzales5071 <32827580+agonzales5071@users.noreply.github.com>
2021-09-01 15:10:31 -0500

    nuked spoondrop
    gonna install react-game-engine making this obsolete

--- src/spoondrop.js
+++ src/spoondrop.js
@@ -1,30 +1,31 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
+
+
+
+
 
 class SpoonDrop extends React.Component {
   render(){
     return (
-      <div className="App">
-
-        <div className="content">
-          <title>Dropped Spoon</title>
-          <h1 id="title" title="&#x1f944;">Alexander <q>DroppedSpoon</q> Gonzales</h1>
-
-          <nav>
-            <a href="resume/">Resume</a> |
-            <a href="spoondrop/">Spoon Drop</a>
-          </nav>
-
-          <p>lorem ipsum or whatever</p>
-          <iframe id="videoplayer" title="Some of My Videos" src="https://www.youtube.com/embed/d9mLhZtSwSs" allowFullScreen={true}>
-          </iframe>
-
-          <br></br>
-
-          {/* <LocationButton id="location" type="button"></LocationButton>
-          <p id="locationtext"></p> */}
-        </div>
+      <div className="App">        
       </div>
       );
     }
 }
+
+// class Board extends React.Component{
+  
+//   constructor(props){
+//     super(props);
+//     this.state = {
+
+//     }
+//   }
+
+//   render(){
+//     return(
+//       <canvas></canvas>
+//     )
+//   }
+// }
 export default SpoonDrop;
