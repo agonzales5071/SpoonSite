7efea201d53586bd95122dea02c23adf5dbb9411
agonzales5071 <32827580+agonzales5071@users.noreply.github.com>
2021-09-14 19:03:37 -0500

    spoons can be dropped
    matter.js implementation

--- src/spoondrop.js
+++ src/spoondrop.js
@@ -1,31 +1,98 @@
 import React, {useRef, useEffect} from "react";
-
-
+import ReactDOM from "react-dom";
+import Matter from "matter-js";
 
 
 
 class SpoonDrop extends React.Component {
-  render(){
-    return (
-      <div className="App">        
-      </div>
-      );
-    }
-}
+  constructor(props) {
+    super(props);
+  }
 
-// class Board extends React.Component{
+  componentDidMount() {
+    var Engine = Matter.Engine,
+      Render = Matter.Render,
+      Composite = Matter.Composite,
+      Bodies = Matter.Bodies,
+      Body = Matter.Body,
+      Mouse = Matter.Mouse,
+      MouseConstraint = Matter.MouseConstraint;
+
+    var engine = Engine.create({
+      // positionIterations: 20
+    });
+
+    var render = Render.create({
+      element: this.refs.scene,
+      engine: engine,
+      options: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        wireframes: false
+      }
+    });
   
-//   constructor(props){
-//     super(props);
-//     this.state = {
+    var ballA = Bodies.circle(210, 100, 30, { restitution: 0.5 });
+    var ballB = Bodies.circle(110, 50, 30, { restitution: 0.5 });
+    var poly = Bodies.polygon(200, 200, 100, 10);
+    var size = 200,
+    x = 200,
+    y = 200,
+    partA = Bodies.rectangle(x, y, size, size / 5),
+    partB = Bodies.rectangle(x, y, size / 5, size, { render: partA.render });
 
-//     }
-//   }
+    var compoundBodyA = Body.create({
+      parts: [partA, partB]
+    });
 
-//   render(){
-//     return(
-//       <canvas></canvas>
-//     )
-//   }
-// }
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+
+    Composite.add(engine.world, [
+      // walls
+      Bodies.rectangle(width/6, height, width/2, 50, { isStatic: true }),
+      Bodies.rectangle(5*width/6, height, width/2, 50, { isStatic: true }),
+      Bodies.rectangle(2*width/3, 0, 50, height/2, { isStatic: true }),
+      Bodies.rectangle(width/3, 0, 50, height/2, { isStatic: true })
+    ]);
+
+    Composite.add(engine.world, [ballA, ballB, compoundBodyA]);
+
+    // add mouse control
+    var mouse = Mouse.create(render.canvas),
+      mouseConstraint = MouseConstraint.create(engine, {
+        mouse: mouse,
+        constraint: {
+          stiffness: 0.2,
+          render: {
+            visible: false
+          }
+        }
+      });
+    
+    Composite.add(engine.world, mouseConstraint);
+
+    Matter.Events.on(mouseConstraint, "mousedown", function(event) {
+      //var poly = Bodies.polygon(200, 200, 100, 10);
+      var size = 100,
+      x = mouse.position.x,
+      y = mouse.position.y,
+      //poly = Bodies.polygon(200, 200, 100, 50),
+      partA = Bodies.circle(x, y-(3*size/5), size/5),
+      partB = Bodies.trapezoid(x, y, size / 5, size, 0.4, { render: partA.render });
+      var compoundBodyA = Body.create({
+        parts: [partA, partB]
+      });
+      Composite.add(engine.world, compoundBodyA);
+    });
+
+    Engine.run(engine);
+
+    Render.run(render);
+  }
+
+  render() {
+    return <div ref="scene" />;
+  }
+}
 export default SpoonDrop;
